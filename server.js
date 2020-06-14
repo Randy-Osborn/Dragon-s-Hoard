bcdda42943c29a96499a797ba02c6c080f36386c
Randy-Osborn <randyosborn1989@hotmail.com>
2020-06-14 11:09:02 -0700

    Init Commit

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require("express")
+const server = express()
+const port = 8080
+server.get("/", (req, res) => {
+    res.status(200).send("Hello World")
+})
+server.listen(port, () => {
+    console.log("server listening at port " + port)
+})
