cc7f7093fc2cb3d98c55c2b491dda8efe04f037c
Randy-Osborn <randyosborn1989@hotmail.com>
2020-06-20 10:49:00 -0700

    second

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
-const express = require("express")
-const server = express()
-const port = 8080
+const express = require("express");
+const server = express();
+const port = 8080;
 server.get("/", (req, res) => {
-    res.status(200).send("Hello World")
-})
+  res.status(200).send("Hello World");
+});
 server.listen(port, () => {
-    console.log("server listening at port " + port)
-})
+  console.log("server listening at port " + port);
+});
