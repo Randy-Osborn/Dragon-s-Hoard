42789653564ba073016d5842949d5dc14b436f07
Randy-Osborn <randyosborn1989@hotmail.com>
2020-06-27 11:32:26 -0700

    Implement a chess board with css and html

--- public/app.js
+++ public/app.js
@@ -1,6 +1,9 @@
 const initButton = document.getElementById("Initiative");
 const theResultSpan = document.getElementById("Result");
-
+const aboutButton = document.getElementById("About");
+aboutButton.addEventListener("click", () => {
+  window.location = "about.html";
+});
 initButton.addEventListener("click", () => {
   const rNum = Math.floor(Math.random() * 20) + 1;
   theResultSpan.textContent = rNum;
