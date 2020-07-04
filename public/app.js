a2a2d7de49cc521d02106213d2864b4342603e8e
Randy-Osborn <randyosborn1989@hotmail.com>
2020-07-04 12:11:57 -0700

    testApi

--- public/app.js
+++ public/app.js
@@ -1,11 +1,33 @@
+const itemResult = document.getElementById("itemResult");
 const initButton = document.getElementById("Initiative");
 const theResultSpan = document.getElementById("Result");
 const aboutButton = document.getElementById("About");
-aboutButton.addEventListener("click", () => {
-  window.location = "about.html";
-});
-initButton.addEventListener("click", () => {
-  const rNum = Math.floor(Math.random() * 20) + 1;
-  theResultSpan.textContent = rNum;
-  console.log(rNum);
+const searchButton = document.getElementById("search");
+//aboutButton.addEventListener("click", () => {
+//  window.location = "about.html";
+//});
+// initButton.addEventListener("click", () => {
+//   const rNum = Math.floor(Math.random() * 20) + 1;
+//   theResultSpan.textContent = rNum;
+//   console.log(rNum);
+// });
+searchButton.addEventListener("click", () => {
+  const itemInput = document.getElementById("item").value;
+  const inputString = "https://www.dnd5eapi.co/api/equipment/" + itemInput;
+  console.log("fetching: " + inputString);
+  fetch(inputString).then((result) => {
+    result.json().then((data) => {
+      const table = document.createElement("table");
+      const itemNameRow = document.createElement("tr");
+      const nameLabel = document.createElement("td");
+      const nameValue = document.createElement("td");
+      table.appendChild(itemNameRow);
+      itemNameRow.appendChild(nameLabel);
+      itemNameRow.appendChild(nameValue);
+      nameLabel.textContent = "Name: ";
+      nameValue.textContent = data.name;
+      itemResult.appendChild(table);
+      console.log(data);
+    });
+  });
 });
--- server.js
+++ server.js
@@ -1,12 +1,17 @@
+const fetch = require("node-fetch");
 const express = require("express");
 const server = express();
 const port = 8080;
 server.use(express.static(__dirname + "/public"));
-/*
-server.get("/", (req, res) => {
-  res.status(200).send("Hello World");
+
+server.get("/items", (req, res) => {
+  fetch("https://www.dnd5eapi.co/api/equipment").then((result) => {
+    result.json().then((data) => {
+      res.status(200).json(data);
+    });
+  });
 });
-*/
+
 server.listen(port, () => {
   console.log("server listening at port " + port);
 });
